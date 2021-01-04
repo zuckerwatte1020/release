76c7428f0ee087501bc15a0cc534dfa182bf257b
watanabe <zuckerwatte1020@outlook.com>
2021-01-04 18:12:58 +0900

    保存

---
+++ WordPress/bistro-calme/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<h2 class="pageTitle">404 NOT FOUND<span>ERROR</span></h2>
+
+<div class="main">
+    <div class="container">
+        <p>お探しのページが見つかりませんでした。</p>
+        <p>申し訳ございませんが、<a href="<?php echo home_url('/'); ?>">こちらのリンク</a>からトップページにお戻りください。</p>
+    </div>
+</div>
+
+<?php get_footer(); ?>
